d5561551c00802fc2eb6d3b1418de5746c71383f
ashleylovette <ashleylovette15@gmail.com>
2022-01-04 20:01:05 -0600

    js navigation

---
+++ src/main.js
@@ -0,0 +1,54 @@
+
+const homeNav = document.getElementById('homeNav');
+const aboutNav = document.getElementById('aboutNav');
+const techStackNav = document.getElementById('techStackNav');
+const portfolioNav = document.getElementById('portfolioNav');
+const contactNav = document.getElementById('contactNav');
+
+// NAVIGATION
+
+
+homeNav.addEventListener("click", e => {
+    homeNav.classList.add("is-active");
+    // remove other classes
+    aboutNav.classList.remove("is-active");
+    techStackNav.classList.remove("is-active");
+    portfolioNav.classList.remove("is-active");
+    contactNav.classList.remove("is-active");
+})
+
+aboutNav.addEventListener("click", e => {
+    aboutNav.classList.add("is-active");
+        // remove other classes
+    homeNav.classList.remove("is-active");
+    techStackNav.classList.remove("is-active");
+    portfolioNav.classList.remove("is-active");
+    contactNav.classList.remove("is-active");
+})
+
+techStackNav.addEventListener("click", e => {
+    techStackNav.classList.add("is-active");
+        // remove other classes
+    homeNav.classList.remove("is-active");
+    aboutNav.classList.remove("is-active");
+    portfolioNav.classList.remove("is-active");
+    contactNav.classList.remove("is-active");
+})
+
+portfolioNav.addEventListener("click", e => {
+    portfolioNav.classList.add("is-active");
+        // remove other classes
+    homeNav.classList.remove("is-active");
+    aboutNav.classList.remove("is-active");
+    techStackNav.classList.remove("is-active");
+    contactNav.classList.remove("is-active");
+})
+
+contactNav.addEventListener("click", e => {
+    contactNav.classList.add("is-active");
+        // remove other classes
+    homeNav.classList.remove("is-active");
+    aboutNav.classList.remove("is-active");
+    techStackNav.classList.remove("is-active");
+    portfolioNav.classList.remove("is-active");
+})
