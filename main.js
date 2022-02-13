ce4ecde4c3315ee3b846c817a03ab20dfc3de6d6
ashleylovette <ashleylovette15@gmail.com>
2022-02-13 13:15:18 -0600

    clean-up

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const aboutNav = document.getElementById('aboutNav');
 const techStackNav = document.getElementById('techStackNav');
 const portfolioNav = document.getElementById('portfolioNav');
 const contactNav = document.getElementById('contactNav');
+const navLinks = document.getElementsByClassName('nav-link')
 
 
 // CLICK NAVIGATION
@@ -11,44 +12,33 @@ const contactNav = document.getElementById('contactNav');
 homeNav.addEventListener("click", e => {
     homeNav.classList.add("is-active");
     // remove other classes
-    aboutNav.classList.remove("is-active");
-    techStackNav.classList.remove("is-active");
-    portfolioNav.classList.remove("is-active");
-    contactNav.classList.remove("is-active");
+    navLinks.classList.remove("is-active");
 })
 
 aboutNav.addEventListener("click", e => {
     aboutNav.classList.add("is-active");
         // remove other classes
-    homeNav.classList.remove("is-active");
-    techStackNav.classList.remove("is-active");
-    portfolioNav.classList.remove("is-active");
-    contactNav.classList.remove("is-active");
+    navLinks.classList.remove("is-active");
+
 })
 
 techStackNav.addEventListener("click", e => {
     techStackNav.classList.add("is-active");
-        // remove other classes
-    homeNav.classList.remove("is-active");
-    aboutNav.classList.remove("is-active");
-    portfolioNav.classList.remove("is-active");
-    contactNav.classList.remove("is-active");
+    //     // remove other classes
+    navLinks.classList.remove("is-active");
+
 })
 
 portfolioNav.addEventListener("click", e => {
     portfolioNav.classList.add("is-active");
         // remove other classes
-    homeNav.classList.remove("is-active");
-    aboutNav.classList.remove("is-active");
-    techStackNav.classList.remove("is-active");
-    contactNav.classList.remove("is-active");
+    navLinks.classList.remove("is-active");
+
 })
 
 contactNav.addEventListener("click", e => {
     contactNav.classList.add("is-active");
         // remove other classes
-    homeNav.classList.remove("is-active");
-    aboutNav.classList.remove("is-active");
-    techStackNav.classList.remove("is-active");
-    portfolioNav.classList.remove("is-active");
+    navLinks.classList.remove("is-active");
+
 })
